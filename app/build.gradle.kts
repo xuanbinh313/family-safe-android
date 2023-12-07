4c3d1b1102c12121219754bcbdb51b3bae36cb07
xuanbinh313 <xuanbinh313@gmail.com>
2023-12-08 01:35:06 +0700

    Complete Step 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.binhcodev.familysafe"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.binhcodev.familysafe"
